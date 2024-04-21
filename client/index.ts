8e968b6d71cbc340104198babffaeea4825a71f8
bogi-hw <167531242+bogi-hw@users.noreply.github.com>
2024-04-21 00:05:30 +0000

    Minor: comment

--- client/index.ts
+++ client/index.ts
@@ -2,6 +2,7 @@ import {RestfuncsClient} from "restfuncs-client";
 import {GreeterSession} from "../GreeterSession.js" // Import to have types
 
 (async () => {
+    // TODO: Why does VS-Code on github workspaces complain on the following line?:
     const greeterSession = new RestfuncsClient<GreeterSession>("/greeterAPI", {/* options */}).proxy
     document.getElementById("main")!.textContent = await greeterSession.greet("Bob")
 })()
