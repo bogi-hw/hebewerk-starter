215465a0e99cca659262fa06308d185939d150de
Boris Gingold <boris.gingold@bogitech.de>
2024-05-16 20:41:18 +0200

    Minor: typescript error fix

--- web/ui-with-react.tsx
+++ web/ui-with-react.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
-import React, {useEffect, useState} from "react";
+import * as React from "react";
+import {useEffect, useState} from "react";
 import { RestfuncsClient } from "restfuncs-client";
 import { GreeterSession } from "../server/GreeterSession.js"; // Import to have types
 
