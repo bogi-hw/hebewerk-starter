16b448b9bf66a711f18c5f03588bee5bfab2fe17
Boris Gingold <boris.gingold@bogitech.de>
2025-07-08 12:22:46 +0200

    - React fix: use createRoot style

--- web/ui-with-react.tsx
+++ web/ui-with-react.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
 import * as React from "react";
+import { createRoot } from 'react-dom/client';
 import {useEffect, useState} from "react";
 import { RestfuncsClient } from "restfuncs-client";
 import { GreeterSession } from "../server/GreeterSession.js"; // Import to have types
@@ -14,7 +14,8 @@ function App() {
     // Die Komponente "App" auf die aktuelle Seite (index.html) rendern:
     const rootDiv = document.createElement("div");
     document.body.append(rootDiv);
-    ReactDOM.render(<App/>, document.getElementById("ui-with-react_container"));
+    const reactRoot = createRoot(rootDiv);
+    reactRoot.render(<App/>)
 
     // **** Eine Funktion auf dem Server aufrufen und Ergebnis ausgeben: ****
     // TODO: Why does VS-Code on github workspaces complain on the following line?:
