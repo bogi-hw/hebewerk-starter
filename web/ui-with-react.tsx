6b79e1f265c780a51351a81c7062f82329b48b74
Boris Gingold <boris.gingold@bogitech.de>
2025-07-08 12:24:02 +0200

    - Add react+MUI example ui code

--- web/ui-with-react.tsx
+++ web/ui-with-react.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import {useEffect, useState} from "react";
+import {watchedComponent, useWatchedState, watched, load, poll, isLoading, loadFailed, preserve, bind, READS_INSIDE_LOADER_FN} from "react-deepwatch"
 import { RestfuncsClient } from "restfuncs-client";
 import { GreeterSession } from "../server/GreeterSession.js"; // Import to have types
+import {Button, Switch, TextField } from "@mui/material";
 
 const greeterSession = new RestfuncsClient<GreeterSession>("/greeterAPI", {/* options */}).proxy
 
-function App() {
-    return <div>Der Kram hier unten / diese Textzeile wurde per React ausgegeben (web/ui-with-react.tsx). {/*TODO: Hier ein paar schöne MUI Elemente einfügen */}</div>
-}
+const App = watchedComponent(props => {
+    const state = useWatchedState({
+        name: "",
+        hasWorm: false,
+    })
+    return <div><br/><br/>Der folgende Kram wurde per React ausgegeben (web/ui-with-react.tsx).<br/><br/>
+        Name des Apfels: <TextField {...bind(state.name)} /><br/>
+        Hat Wurm: <Switch {...bind(state.hasWorm)}/><br/>
+        <Button onClick={async () => await greeterSession.addAnApple(state.hasWorm) }>Apfel in den Korb legen</Button>
+    </div>
+});
 
 (async () => {
     // Die Komponente "App" auf die aktuelle Seite (index.html) rendern:
@@ -21,7 +31,4 @@ function App() {
     // TODO: Why does VS-Code on github workspaces complain on the following line?:
 
     console.log(await greeterSession.greet("Bob"));
-
-
-    await greeterSession.addAnApple(false);
 })()
